b3c2bcb40788f700be8a3d98571cd425482e725d
DaniloBorreros <cvsubacoor.mis@cvsu.edu.ph>
2025-04-08 18:05:46 +0800

    style: remove unnecessary comment in StudentCard component

--- components/StudentCard.tsx
+++ components/StudentCard.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-//asdsadsad
-
 import { useState, useEffect } from "react";
 import { LibraryBig, Cloud, Sun, CloudRain } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
